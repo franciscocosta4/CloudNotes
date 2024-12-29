3bd3614f41896739176ec3f2d86bf8117488eeeb
Francisco da silva costa <80585387+franciscocosta4@users.noreply.github.com>
2024-12-29 11:33:45 +0000

    13- configuração do scout

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model
 {
-    //* NECESSÁRIO PARA O SCOUT
-    use Searchable;
-
     protected $fillable = [
         'user_id', 
         'title', 
@@ -18,9 +15,28 @@ class Note extends Model
         'file_path',
     ];
 
+
     // Relacionamento com o modelo User
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
+    //* NECESSÁRIO PARA O SCOUT
+    use Laravel\Scout\Searchable;
+
+    use Searchable;
+
+    public function toSearchableArray()
+    {
+        return [
+            'user_name' => $this->user->name,
+            'title' => $this->title,
+            'subject'=>$this->subject,
+            'topic_difficulty'=>$this->topic_difficulty,
+            'content' => $this->content,
+            'file_path'=> $this->file_path,
+        ];
+    }
+
 }
